d8aa296a251f535b8887d0904574a0fa59483c82
daominhtan <daominh.tan93@gmail.com>
2024-12-19 00:34:01 +0700

    Initial code

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pong v2",
 		})
 	})
 	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "ok")
+	})
+	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "ok")
 	})
 	r.Run()
