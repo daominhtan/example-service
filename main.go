d80f7f082f2cd53d6352a11cd2ec239eff01653c
daominhtan <daominh.tan93@gmail.com>
2024-12-25 23:20:55 +0700

    add v3

--- main.go
+++ main.go
@@ -8,9 +8,14 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "version: 3",
+		})
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong v2",
+			"message": "pong v3",
 		})
 	})
 	r.GET("/health", func(c *gin.Context) {
